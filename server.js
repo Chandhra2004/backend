5e2bdf6d0c2bddeb42fe7d175c51e13df42a54ed
Chandhra <chandhra.guduru@gmail.com>
2025-04-17 22:46:49 +0530

    Fixed Issues

--- server.js
+++ server.js
@@ -190,7 +190,7 @@ app.use((err, req, res, next) => {
 
 
 server.listen(process.env.PORT, "0.0.0.0", () => {
-  console.log(`✅ Server running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
 });
 
 module.exports = app;
